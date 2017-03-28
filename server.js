ed9196b7f8a3a118447bfe1c4172a58fe8ec0f3d
Wisbell <westleyisbell@gmail.com>
2017-03-28 11:42:50 -0500

    Created basic files

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const express = require('express');
 const app = express();
 
 
-app.use(express.static('public'));
+let popularPastries = {
+  donut: 30,
+  bread: 20,
+  bagle: 10
+}
+
+
+app.use(express.static('/'));
 
 // set pug view engine
 app.set('view engine', 'pug')
@@ -12,6 +19,14 @@ app.set('view engine', 'pug')
 
 app.get('/', (req, res, next) => {
   res.render('index')
+})
+
+app.get('/about', (req, res, next) => {
+  res.render('about')
+})
+
+app.get('/inventory', (req, res, next) => {
+  res.render('inventory', { popularPastries })
 })
 
 const port = process.env.PORT || 3000
